2430e735f161d215224b78f915de3fb9560d48dd
Donavan Lawson <donavan@nonacreative.com>
2017-10-29 16:59:39 +0200

    wip

--- bin/config.sh
+++ bin/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SERVICE_NAME=ImageCompressor
-S3_BUCKET=lambda-image-compress
+S3_BUCKET=lambda-image-compress-bucket
 AWS_PROFILE=default
 
 AWS_USER=$(aws iam get-user --profile ${AWS_PROFILE} | grep UserName | awk '{print $2}' | sed 's/[",]//g')
