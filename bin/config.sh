dbde9d3005e90506b9f37a3ce54b501014744d88
Donavan Lawson <donavan@nonacreative.com>
2017-11-01 19:42:25 +0200

    working

--- bin/config.sh
+++ bin/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SERVICE_NAME=ImageCompressor
-S3_BUCKET=orderin-image-resize-bucket
+S3_BUCKET=orderin-image-resize-resources
 AWS_PROFILE=orderin
 
 AWS_USER=$(aws iam get-user --profile ${AWS_PROFILE} | grep UserName | awk '{print $2}' | sed 's/[",]//g')
