a7543bb536059c381b3d6619ccfbdd4ea75dd53a
Donavan Lawson <donavan@nonacreative.com>
2017-11-04 23:40:25 +0200

    account prefix added

--- bin/config.sh
+++ bin/config.sh
@@ -5,7 +5,7 @@ S3_BUCKET=lambda-image-compress-bucket
 AWS_PROFILE=default-donavan-aws
 
 AWS_USER=$(aws iam get-user --profile ${AWS_PROFILE} | grep UserName | awk '{print $2}' | sed 's/[",]//g')
-ACCOUNT_PREFIX=OrderIn-${AWS_USER}
+ACCOUNT_PREFIX=dev-${AWS_USER}
 
 S3_PREFIX=${SERVICE_NAME}
 STACK_NAME=${SERVICE_NAME}
